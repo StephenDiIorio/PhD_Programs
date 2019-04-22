b0646fff8b9a5aa78cbc5c39af807a92a01f1ce8
Stephen DiIorio <PlasmicQuark@users.noreply.github.com>
2019-04-22 03:27:43 -0400

    add dummy to unused variables, cart hi-order calc

--- Visualization_Tools/2fluid_terms_radial.py
+++ Visualization_Tools/2fluid_terms_radial.py
@@ -255,11 +255,11 @@ def Generalized_Ohm_radavg(sdfdata, species=None):
 
     # plt.savefig('v5.png', dpi=600, bbox_inches="tight")
 
-    avg_3, r, t = reproject_image_into_polar(v_3_dist, origin=None, Jacobian=False, dr=1, dt=None)
+    avg_3, dummy_r, dummy_t = reproject_image_into_polar(v_3_dist, origin=None, Jacobian=False, dr=1, dt=None)
     o_ma = np.ma.masked_equal(avg_3, 0.)
     avg_3 = np.average(o_ma, axis=1)
 
-    avg_5, r, t = reproject_image_into_polar(v_5_dist, origin=None, Jacobian=False, dr=1, dt=None)
+    avg_5, dummy_r, dummy_t = reproject_image_into_polar(v_5_dist, origin=None, Jacobian=False, dr=1, dt=None)
     o_ma = np.ma.masked_equal(avg_5, 0.)
     avg_5 = np.average(o_ma, axis=1)
 
@@ -275,9 +275,9 @@ def __radial_average(x_grid, y_grid, field_x, field_y):
     R, T = cart2polar(X, Y)
 
     er = np.multiply(field_x, np.cos(T)) + np.multiply(field_y, np.sin(T))
-    et = np.multiply(field_y, np.cos(T)) - np.multiply(field_x, np.sin(T))
+    dummy_et = np.multiply(field_y, np.cos(T)) - np.multiply(field_x, np.sin(T))
 
-    o, r, t = reproject_image_into_polar(er, origin=None, Jacobian=False, dr=1, dt=None)
+    o, dummy_r, dummy_t = reproject_image_into_polar(er, origin=None, Jacobian=False, dr=1, dt=None)
 
     o_ma = np.ma.masked_equal(o, 0.)
     avg = np.average(o_ma, axis=1)
@@ -304,32 +304,42 @@ def higher_order(sdfdata, species=None):
     p_list = list(zip(p_pos[0], p_pos[1]))
 
 
+    limit = 1e7
+
     v_r = np.sqrt(vx**2 + vy**2)# + vz**2) #NOTE: might need to get rid of vz. can then use cart2polar for v_r and v_t
     v_r_dist = first_order_weight_2d(x, y, dx, dy, p_list, weight=w, values=v_r)
-    avg_r, r, t = reproject_image_into_polar(v_r_dist, origin=None, Jacobian=False, dr=1, dt=None)
+    avg_r, r, dummy_t = reproject_image_into_polar(v_r_dist, origin=None, Jacobian=False, dr=1, dt=None)
     o_ma = np.ma.masked_equal(avg_r, 0.)
     avg_r = np.average(o_ma, axis=1)
-    fig1, ax1 = plt.subplots()
-    ax1.plot(avg_r)
+
+    fig1, ax1 = plt.subplots(2, sharex=True)
+    ax1[0].plot(avg_r)
+    ax1[1].plot(np.clip(avg_r, -limit, limit))
     fig1.savefig('vr.png', dpi=600, bbox_inches="tight")
+
 
     v_t = np.arctan2(vx, vy)
     v_t_dist = first_order_weight_2d(x, y, dx, dy, p_list, weight=w, values=v_t)
     avg_t, r, t = reproject_image_into_polar(v_t_dist, origin=None, Jacobian=False, dr=1, dt=None)
     o_ma = np.ma.masked_equal(avg_t, 0.)
     avg_t = np.average(o_ma, axis=1)
-    fig2, ax2 = plt.subplots()
-    ax2.plot(avg_t)
+
+    fig2, ax2 = plt.subplots(2, sharex=True)
+    ax2[0].plot(avg_t)
+    ax2[1].plot(np.clip(avg_t, -limit, limit))
     fig2.savefig('vt.png', dpi=600, bbox_inches="tight")
+
 
     v_3 = (np.sqrt(vx**2 + vy**2 + vz**2))**3
     v_3_dist = first_order_weight_2d(x, y, dx, dy, p_list, weight=w, values=v_3)
     avg_3, r, t = reproject_image_into_polar(v_3_dist, origin=None, Jacobian=False, dr=1, dt=None)
     o_ma = np.ma.masked_equal(avg_3, 0.)
     avg_3 = np.average(o_ma, axis=1)
-    fig3, ax3 = plt.subplots()
-    ax3.plot(avg_3)
-    fig3.savefig('v3.png', dpi=600, bbox_inches="tight")
+
+    fig3, ax3 = plt.subplots(2, sharex=True)
+    ax3[0].plot(avg_3)
+    ax3[1].plot(np.clip(avg_3, -limit, limit))
+    fig3.savefig('avg_v3.png', dpi=600, bbox_inches="tight")
 
 
     r = np.linspace(0.0, np.max(np.sqrt(x**2 + y**2)), num=avg_r.size)
@@ -338,31 +348,190 @@ def higher_order(sdfdata, species=None):
     num_1 = np.multiply(r, avg_3)
     num_1 = np.multiply(avg_r, num_1)
     num_1 = np.multiply(avg_r, num_1)
+
+    fig4, ax4 = plt.subplots(2, sharex=True)
+    ax4[0].plot(num_1)
+    ax4[1].plot(np.clip(num_1, -limit, limit))
+    fig4.savefig('num1_before_grad', dpi=600, bbox_inches="tight")
+
     num_1 = np.gradient(num_1, dx) #TODO: what to use for grid spacing radially
-    fig4, ax4 = plt.subplots()
-    ax4.plot(num_1)
-    fig4.savefig('num1.png', dpi=600, bbox_inches="tight")
+
+    fig5, ax5 = plt.subplots(2, sharex=True)
+    ax5[0].plot(num_1)
+    ax5[1].plot(np.clip(num_1, -limit, limit))
+    fig5.savefig('num1.png', dpi=600, bbox_inches="tight")
+
 
     num_2 = np.multiply(avg_r, avg_t)
     num_2 = np.multiply(avg_3, num_2)
+
+    fig6, ax6 = plt.subplots(2, sharex=True)
+    ax6[0].plot(num_2)
+    ax6[1].plot(np.clip(num_2, -limit, limit))
+    fig6.savefig('num2_before_grad.png', dpi=600, bbox_inches="tight")
+
     num_2 = np.gradient(num_2, max(x.size, y.size)) #TODO: what to use for grid spacing theta
-    fig5, ax5 = plt.subplots()
-    ax5.plot(num_2)
-    fig5.savefig('num2.png', dpi=600, bbox_inches="tight")
+
+    fig7, ax7 = plt.subplots(2, sharex=True)
+    ax7[0].plot(num_2)
+    ax7[1].plot(np.clip(num_2, -limit, limit))
+    fig7.savefig('num2.png', dpi=600, bbox_inches="tight")
+
 
     # num = np.divide(num_1 + num_2, r, where=r!=0.0)
     num = np.divide(num_1, r, where=r!=0.0)
-    fig6, ax6 = plt.subplots()
-    ax6.plot(num)
-    fig6.savefig('num.png', dpi=600, bbox_inches="tight")
+    fig8, ax8 = plt.subplots(2, sharex=True)
+    ax8[0].plot(num)
+    ax8[1].plot(np.clip(num, -limit, limit))
+    fig8.savefig('num.png', dpi=600, bbox_inches="tight")
 
     const = -sc.m_e / (2 * sc.e)
     final = const * np.divide(num, avg_3, where=avg_3!=0.0)
-    fig7, ax7 = plt.subplots()
-    ax7.plot(final)
-    fig7.savefig('final.png', dpi=600, bbox_inches="tight")
+    fig9, ax9 = plt.subplots(2, sharex=True)
+    ax9[0].plot(final)
+    ax9[1].plot(np.clip(final, -limit, limit))
+    fig9.savefig('final.png', dpi=600, bbox_inches="tight")
 
     return final#const * np.divide(num, avg_3, where=avg_3!=0.0)
+
+
+def cart_higher_order_x(sdfdata, species=None):
+    # grid = sdfdata.__dict__["Grid_Grid_mid"].data
+    grid = sdfdata.__dict__["Grid_Grid"].data
+
+    x = grid[0]
+    y = grid[1]
+
+    dx = grid[0][1] - grid[0][0]
+    dy = grid[1][1] - grid[1][0]
+
+    p_pos = sdfdata.__dict__[get_varname("Grid_Particles", species)].data
+    vx = sdfdata.__dict__[get_varname("Particles_Vx", species)].data
+    vy = sdfdata.__dict__[get_varname("Particles_Vy", species)].data
+    vz = sdfdata.__dict__[get_varname("Particles_Vz", species)].data
+    w = sdfdata.__dict__[get_varname("Particles_Weight", species)].data
+
+    v_3 = (np.sqrt(vx**2 + vy**2 + vz**2))**3
+
+    p_list = list(zip(p_pos[0], p_pos[1]))
+
+
+    v_3_dist = first_order_weight_2d(x, y, dx, dy, p_list, weight=w, values=v_3)
+    vx_dist  = first_order_weight_2d(x, y, dx, dy, p_list, weight=w, values=vx)
+    vy_dist  = first_order_weight_2d(x, y, dx, dy, p_list, weight=w, values=vy)
+
+    fig1, ax1 = plt.subplots()
+    ax1.pcolor(v_3_dist)
+    fig1.colorbar()
+    fig1.savefig('v3.png', dpi=600, bbox_inches="tight")
+    fig2, ax2 = plt.subplots()
+    ax2.pcolor(vx_dist)
+    fig2.colorbar()
+    fig2.savefig('vx.png', dpi=600, bbox_inches="tight")
+    fig3, ax3 = plt.subplots()
+    ax3.pcolor(vy_dist)
+    fig3.colorbar()
+    fig3.savefig('vy.png', dpi=600, bbox_inches="tight")
+
+    term1 = np.multiply(vx_dist, vx_dist)
+    term1 = np.multiply(term1, v_3_dist)
+    fig4, ax4 = plt.subplots()
+    ax4.pcolor(term1)
+    fig4.colorbar()
+    fig4.savefig('term1_cartx_before_grad.png', dpi=600, bbox_inches="tight")
+
+    term1 = np.gradient(term1, dx, axis=0)
+    fig5, ax5 = plt.subplots()
+    ax5.pcolor(term1)
+    fig5.colorbar()
+    fig5.savefig('term1_cartx.png', dpi=600, bbox_inches="tight")
+
+    term2 = np.multiply(vx_dist, vy_dist)
+    term2 = np.multiply(term2, v_3_dist)
+    fig6, ax6 = plt.subplots()
+    ax6.pcolor(term2)
+    fig6.colorbar()
+    fig6.savefig('term2_cartx_before_grad.png', dpi=600, bbox_inches="tight")
+
+    term2 = np.gradient(term2, dy, axis=1)
+    fig7, ax7 = plt.subplots()
+    ax7.pcolor(term2)
+    fig7.colorbar()
+    fig7.savefig('term2_cartx.png', dpi=600, bbox_inches="tight")
+
+    div_num = term1 + term2
+
+    const = -sc.m_e / (2 * sc.e)
+    final = const * np.divide(div_num, v_3_dist, where=v_3_dist!=0.0)
+    fig8, ax8 = plt.subplots()
+    ax8.pcolor(final)
+    fig8.colorbar()
+    fig8.savefig('final_cartx.png', dpi=600, bbox_inches="tight")
+
+    return final
+
+
+def cart_higher_order_y(sdfdata, species=None):
+    # grid = sdfdata.__dict__["Grid_Grid_mid"].data
+    grid = sdfdata.__dict__["Grid_Grid"].data
+
+    x = grid[0]
+    y = grid[1]
+
+    dx = grid[0][1] - grid[0][0]
+    dy = grid[1][1] - grid[1][0]
+
+    p_pos = sdfdata.__dict__[get_varname("Grid_Particles", species)].data
+    vx = sdfdata.__dict__[get_varname("Particles_Vx", species)].data
+    vy = sdfdata.__dict__[get_varname("Particles_Vy", species)].data
+    vz = sdfdata.__dict__[get_varname("Particles_Vz", species)].data
+    w = sdfdata.__dict__[get_varname("Particles_Weight", species)].data
+
+    v_3 = (np.sqrt(vx**2 + vy**2 + vz**2))**3
+
+    p_list = list(zip(p_pos[0], p_pos[1]))
+
+
+    v_3_dist = first_order_weight_2d(x, y, dx, dy, p_list, weight=w, values=v_3)
+    vx_dist  = first_order_weight_2d(x, y, dx, dy, p_list, weight=w, values=vx)
+    vy_dist  = first_order_weight_2d(x, y, dx, dy, p_list, weight=w, values=vy)
+
+    term1 = np.multiply(vy_dist, vx_dist)
+    term1 = np.multiply(term1, v_3_dist)
+    fig4, ax4 = plt.subplots()
+    ax4.pcolor(term1)
+    fig4.colorbar()
+    fig4.savefig('term1_carty_before_grad.png', dpi=600, bbox_inches="tight")
+
+    term1 = np.gradient(term1, dx, axis=0)
+    fig5, ax5 = plt.subplots()
+    ax5.pcolor(term1)
+    fig5.colorbar()
+    fig5.savefig('term1_carty.png', dpi=600, bbox_inches="tight")
+
+    term2 = np.multiply(vy_dist, vy_dist)
+    term2 = np.multiply(term2, v_3_dist)
+    fig6, ax6 = plt.subplots()
+    ax6.pcolor(term2)
+    fig6.colorbar()
+    fig6.savefig('term2_carty_before_grad.png', dpi=600, bbox_inches="tight")
+
+    term2 = np.gradient(term2, dy, axis=1)
+    fig7, ax7 = plt.subplots()
+    ax7.pcolor(term2)
+    fig7.colorbar()
+    fig7.savefig('term2_carty.png', dpi=600, bbox_inches="tight")
+
+    div_num = term1 + term2
+
+    const = -sc.m_e / (2 * sc.e)
+    final = const * np.divide(div_num, v_3_dist, where=v_3_dist!=0.0)
+    fig8, ax8 = plt.subplots()
+    ax8.pcolor(final)
+    fig8.colorbar()
+    fig8.savefig('final_carty.png', dpi=600, bbox_inches="tight")
+
+    return final
 
 
 def main():
@@ -396,26 +565,32 @@ def main():
         e_var_y = sdfdata.Electric_Field_Ey
         x_grid = e_var_x.grid_mid.data[0]
         y_grid = e_var_y.grid_mid.data[1]
+        x_grid_cent = e_var_x.grid.data[0]
+        y_grid_cent = e_var_y.grid.data[1]
 
-        e_avg, e_r, e_t = __radial_average(x_grid, y_grid, e_var_x.data, e_var_y.data)
+        e_avg, dummy_e_r, dummy_e_t = __radial_average(x_grid, y_grid, e_var_x.data, e_var_y.data)
 
-        temp_grid_x, temp_data_x = Temp_Field(sdfdata,
-                                              x_axis_num,
-                                              species='Electron')
-        temp_grid_y, temp_data_y = Temp_Field(sdfdata,
-                                              y_axis_num,
-                                              species='Electron')
-        temp_avg, temp_r, temp_t = __radial_average(x_grid, y_grid, temp_data_x, temp_data_y)
+        dummy_temp_grid_x, temp_data_x = Temp_Field(sdfdata,
+                                                    x_axis_num,
+                                                    species='Electron')
+        dummy_temp_grid_y, temp_data_y = Temp_Field(sdfdata,
+                                                    y_axis_num,
+                                                    species='Electron')
+        temp_avg, dummy_temp_r, dummy_temp_t = __radial_average(x_grid, y_grid,
+                                                                temp_data_x,
+                                                                temp_data_y)
 
-        res_mhd_grid_x, res_mhd_data_x = Resistive_MHD_Field(sdfdata,
-                                                             x_axis_num)
-        res_mhd_grid_y, res_mhd_data_y = Resistive_MHD_Field(sdfdata,
-                                                             y_axis_num)
-        rmhd_avg, rmhd_r, rmhd_t = __radial_average(x_grid, y_grid, res_mhd_data_x, res_mhd_data_y)
+        dummy_res_mhd_grid_x, res_mhd_data_x = Resistive_MHD_Field(sdfdata,
+                                                                   x_axis_num)
+        dummy_res_mhd_grid_y, res_mhd_data_y = Resistive_MHD_Field(sdfdata,
+                                                                   y_axis_num)
+        rmhd_avg, dummy_rmhd_r, dummy_rmhd_t = __radial_average(x_grid, y_grid,
+                                                                res_mhd_data_x,
+                                                                res_mhd_data_y)
 
-        hall_grid_x, hall_data_x = Hall_Field(sdfdata, x_axis_num)
-        hall_grid_y, hall_data_y = Hall_Field(sdfdata, y_axis_num)
-        hall_avg, hall_r, hall_t = __radial_average(x_grid, y_grid, hall_data_x, hall_data_y)
+        dummy_hall_grid_x, hall_data_x = Hall_Field(sdfdata, x_axis_num)
+        dummy_hall_grid_y, hall_data_y = Hall_Field(sdfdata, y_axis_num)
+        hall_avg, dummy_hall_r, dummy_hall_t = __radial_average(x_grid, y_grid, hall_data_x, hall_data_y)
 
         gen_data_x = Generalized_Ohm(sdfdata,
                                      x_axis_num,
@@ -423,12 +598,16 @@ def main():
         gen_data_y = Generalized_Ohm(sdfdata,
                                      y_axis_num,
                                      species='Electron')
-        gen_avg, gen_r, gen_t = __radial_average(x_grid, y_grid, gen_data_x, gen_data_y)
+        gen_avg, dummy_gen_r, dummy_gen_t = __radial_average(x_grid, y_grid, gen_data_x, gen_data_y)
 
         gen_avg2 = Generalized_Ohm_radavg(sdfdata,
                                           species='Electron')
 
         high_order = higher_order(sdfdata, species='Electron')
+
+        cart_high_x = cart_higher_order_x(sdfdata, specied='Electron')
+        cart_high_y = cart_higher_order_y(sdfdata, species='Electron')
+        cart_high_avg, dummy_high_r, dummy_high_t = __radial_average(x_grid_cent, y_grid_cent, cart_high_x, cart_high_y)
 
         # ideal_mhd_grid_x, ideal_mhd_data_x = Ideal_MHD_Field(sdfdata,
         #                                                      x_axis_num,
@@ -470,8 +649,11 @@ def main():
         l8, = axarr[i].plot(np.clip(high_order, -limit, limit),
                             'g:',
                             label='Higher Order')
+        l9, = axarr[i].plot(np.clip(cart_high_avg, -limit, limit),
+                            'b:',
+                            label='Cart Higher Order')
 
-    ls = [l1, l2, l6, l7, l8]  #l3, l4, l5]
+    ls = [l1, l2, l6, l7, l8, l9]  #l3, l4, l5]
     labels = [l.get_label() for l in ls]
     lgd = fig.legend(ls, labels, bbox_to_anchor=(1.05, 1.0), loc=1)
     fig.subplots_adjust(hspace=0)
