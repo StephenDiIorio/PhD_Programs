3cbc0e1d444a469fa21efb54154d83377bb9e8ed
Stephen DiIorio <PlasmicQuark@users.noreply.github.com>
2019-07-04 13:43:22 -0600

    update func def and remove includes

--- Particle_Tracker/Cpp/partrac.h
+++ Particle_Tracker/Cpp/partrac.h
@@ -2,22 +2,17 @@
   --------------------------------------------------
   PARTRAC v1.0: 3D particle tracking code
   AGRT 2010
-  Moderized by Stephen DiIorio 2019
+  Modernized by Stephen DiIorio 2019
 
   Function list + headers
   --------------------------------------------------
 */
 
-#include <iostream>
 #include <cmath>
 #include <fstream>
 #include <stdlib.h>
 #include <time.h>
 #include "constants.h"
-
-/*#include "init.h"
-#include "weighting.h"
-#include "motion.h"*/
 
 int initpos(double **pos);
 int initmom(double **mom);
@@ -30,3 +25,4 @@ int WeightF(double ****EVec, double **parVec, double **FVec);
 int pushmomentum(double **FVec, double **momVec, const double dt_m);
 int pushxpos(double **momVec, double **parVec, const double dt_m);
 int Lorentz(double **momVec, double **BVec, const double dt_m);
+int singlepushxpos(double **momVec, double **parVec, const double dist);
