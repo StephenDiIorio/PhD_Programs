b464784d1758b47b4e1ade299eda54c5492900ac
Stephen DiIorio <PlasmicQuark@users.noreply.github.com>
2019-07-04 16:55:53 -0600

    alter to loop over several input file

--- Particle_Tracker/Cpp/main.cpp
+++ Particle_Tracker/Cpp/main.cpp
@@ -20,7 +20,13 @@
 #include "partrac.h"
 
 int main(int argnum, char **argstr) {
-    unsigned long i, j, x1;
+    unsigned long i, j, x1, f;
+    const double tmax = 125; //sqrt(2.0) * 2.0 * PI;
+    double t = 0;
+    const double dt = 0.01;
+    const double dist = 143002.;
+    bool outsideDomain;
+
     double start_time = clock();
 
     //----------------------------------------
@@ -29,8 +35,31 @@ int main(int argnum, char **argstr) {
     printf(version);
     printf(" ----\n\n");
 
-    // for file output
-    FILE *outFile = fopen(filename, "w");
+    //----------------------------------------
+    // For File Output
+    FILE *trajOutFile = fopen(trajFilename, "w");
+    FILE *sliceOutFile = fopen(sliceFilename, "w");
+
+    //----------------------------------------
+    // Arrays and counters for data dumps
+    unsigned long t_count;
+    static double *t_array;
+    t_array = new double[(unsigned long)(tmax / dt)];
+
+    static double ***pos_data;
+    pos_data = new double **[(unsigned long)(tmax / dt)];
+    for (i = 0; i < (unsigned long)(tmax / dt); ++i) {
+        pos_data[i] = new double *[Npar];
+        for (j = 0; j < Npar; ++j) {
+            pos_data[i][j] = new double [ndims];
+        }
+    }
+
+    static double **finalYPos;
+    finalYPos = new double *[numFiles];
+    for (f = 0; f < numFiles; ++f) {
+        finalYPos[f] = new double [Npar];
+    }
 
     //----------------------------------------
     // Particle positions and momenta
@@ -68,58 +97,61 @@ int main(int argnum, char **argstr) {
         }
     }
 
-    // initialize particles
-    initpos(pos);
-    initmom(mom);
+    // NOTE: Even if input files are same, will get different results
+    // becuase of rand() in pos initialization. Mitigate this by calling
+    // srand(1) in initpos.
+    for (f = 0; f < numFiles; ++f) {
+        sprintf(xInput, "x%lu_field.dat", f);
+        sprintf(yInput, "y%lu_field.dat", f);
+        sprintf(zInput, "z%lu_field.dat", f);
 
-    //initialize grids
-    initEfieldFromFile(Efield);
-    initBfield(Bfield);
-    WeightF(Efield, pos, fce);
-    WeightF(Bfield, pos, bpa);
+        // initialize particles
+        initpos(pos);
+        initmom(mom);
 
-    const double tmax = 125;//sqrt(2.0) * 2.0 * PI;
-    double t = 0;
-    const double dt = 0.01;
-
-    const double dist = 143002.;
-
-    bool outsideDomain;
-
-    //----------------------------------------
-    // Arrays and counters for data dumps
-    uint t_count = 0;
-    static double *t_array;
-    t_array = new double[(uint)(tmax / dt)];
-
-    static double ***pos_data;
-    pos_data = new double **[(uint)(tmax / dt)];
-    for (i = 0; i < (uint)(tmax / dt); ++i) {
-        pos_data[i] = new double *[Npar];
-        for (j = 0; j < Npar; ++j) {
-            pos_data[i][j] = new double [ndims];
-        }
-    }
-
-    //----------------------------------------
-    // Main loop
-    while (t<=tmax) {
-        //Weight Electric fields to particles
+        //initialize grids
+        initEfieldFromFile(Efield, xInput, yInput, zInput);
+        initBfield(Bfield);
         outsideDomain = WeightF(Efield, pos, fce);
         outsideDomain = WeightF(Bfield, pos, bpa);
 
-        if (outsideDomain) {
-            printf("Breaking time loop. All particles are out of box. t=%f\n", t);
-            break;
+        t = 0;
+        t_count = 0;
+
+        //----------------------------------------
+        // Main loop
+        while (t<=tmax) {
+            //Weight Electric fields to particles
+            outsideDomain = WeightF(Efield, pos, fce);
+            outsideDomain = WeightF(Bfield, pos, bpa);
+
+            if (outsideDomain) {
+                printf("Breaking time loop. All particles are out of box. t=%f\n", t);
+                break;
+            }
+
+            // Equations of motion
+            pushmomentum(fce, mom, dt * 0.5);
+            Lorentz(mom, bpa, dt);
+            pushmomentum(fce, mom, dt * 0.5);
+            pushxpos(mom, pos, dt);
+
+            // Store data
+            t_array[t_count] = t;
+            for (i = 0; i < Npar; ++i) {
+                for (x1 = 0; x1 < ndims; ++x1) {
+                    pos_data[t_count][i][x1] = pos[x1][i];
+                }
+            }
+            ++t_count;
+
+            t += dt;
         }
 
-        // Equations of motion
-        pushmomentum(fce, mom, dt * 0.5);
-        Lorentz(mom, bpa, dt);
-        pushmomentum(fce, mom, dt * 0.5);
-        pushxpos(mom, pos, dt);
+        printf("Were all the particles outside the domain?: %i (0=No, 1=Yes)\n", outsideDomain);
 
-        // Store data
+        // Final push to target distance and data dump
+        singlepushxpos(mom, pos, dist);
         t_array[t_count] = t;
         for (i = 0; i < Npar; ++i) {
             for (x1 = 0; x1 < ndims; ++x1) {
@@ -128,34 +160,32 @@ int main(int argnum, char **argstr) {
         }
         ++t_count;
 
-        t += dt;
-    }
-
-    printf("Were all the particles outside the domain?: %i (0=No, 1=Yes)\n", outsideDomain);
-
-    // Final push to target distance and data dump
-    singlepushxpos(mom, pos, dist);
-    t_array[t_count] = t;
-    for (i = 0; i < Npar; ++i) {
-        for (x1 = 0; x1 < ndims; ++x1) {
-            pos_data[t_count][i][x1] = pos[x1][i];
+        for (i = 0; i < Npar; ++i) {
+            finalYPos[f][i] = pos[yIndx][i];
         }
     }
-    ++t_count;
 
     //----------------------------------------
     // Write data to file
     for (i = 0; i < t_count; ++i) {
-        fprintf(outFile, "%e", t_array[i]);
+        fprintf(trajOutFile, "%e", t_array[i]);
         for (j = 0; j < Npar; ++j) {
-            fprintf(outFile, "\t%e\t%e\t%e", pos_data[i][j][xIndx], pos_data[i][j][yIndx], pos_data[i][j][zIndx]);
+            fprintf(trajOutFile, "\t%e\t%e\t%e", pos_data[i][j][xIndx], pos_data[i][j][yIndx], pos_data[i][j][zIndx]);
         }
-        fprintf(outFile, "\n");
+        fprintf(trajOutFile, "\n");
+    }
+
+    for (f = 0; f < numFiles; ++f) {
+        for (i = 0; i < Npar; ++i) {
+            fprintf(sliceOutFile, "%f\t", finalYPos[f][i]);
+        }
+        fprintf(sliceOutFile, "\n");
     }
 
     //----------------------------------------
     // Finish
-    fclose(outFile);
+    fclose(trajOutFile);
+    fclose(sliceOutFile);
     double end_time = clock();
     printf("finished\n");
     printf("\nEND OF CALCULATION\n");
@@ -171,6 +201,11 @@ int main(int argnum, char **argstr) {
     }
     delete[] pos_data;
     delete[] t_array;
+
+    for (f = 0; f < numFiles; ++f) {
+        delete[] finalYPos[f];
+    }
+    delete[] finalYPos;
 
     for (x1 = 0; x1 < ndims; ++x1) {
         for (i = 0; i < Nx; ++i) {
